203d7478e6e1afe2712c83bfd6153f0aaadb3d16
Wayne Starr <Racer159@users.noreply.github.com>
2024-10-09 15:37:24 -0600

    chore: update to `uds-common` v1 (#117)
    ## Description
    
    This updates the repo to `uds-common` v1
    ## Related Issue
    
    Fixes #N/A
    
    ## Type of change
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [X] Other (security config, docs update, etc)
    
    ## Checklist before merging
    
    - [X] Test, docs, adr added or updated as needed
    - [X] [Contributor Guide
    Steps](https://github.com/defenseunicorns/uds-package-sonarqube/blob/main/CONTRIBUTING.md#developer-workflow)
    followed

--- src/monitoring-image/curl.sh
+++ src/monitoring-image/curl.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-cp /jmx_prometheus_javaagent-$JMX_PROMETHEUS_PLUGIN_VERSION.jar /data/jmx_prometheus_javaagent.jar
+cp "/jmx_prometheus_javaagent-$JMX_PROMETHEUS_PLUGIN_VERSION.jar" /data/jmx_prometheus_javaagent.jar
 
 echo "finished loading jmx prometheus plugin"
